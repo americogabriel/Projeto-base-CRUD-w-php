092a1c1f8e5bc161eedf4f7fecb64cddd49d175b
Gabriel Ferrreira <gabrieldouble007@gmail.com>
2025-10-28 17:54:11 -0300

    Banco de Dados Adcionado com Sucesso e programa funcionando

--- cadastracliente.php
+++ cadastracliente.php
@@ -2,7 +2,21 @@
     include_once("conectaMYSQL.php");
     include_once("form.php");
 
-    
+    if ($_SERVER['REQUEST_METHOD']== "POST"){
+        $nome = $_POST['nome'];
+        $pais = $_POST['pais'];
 
+        $sql = "INSERT INTO pessoas (nome,pais) VALUES ('$nome','$pais')";
+
+        $adcionar = mysqli_query($connect,$sql);
+
+        if (!$adcionar){
+            echo "<h2> Não foi Possível adcionar o novo usuário no banco.";
+        }
+        else{
+            echo "<h2> Novo usuário Adcionado com Sucesso no Banco de Dados";
+        }
+    }
+    echo "<a href='index.php'>Voltar</a>";
 
 ?>
